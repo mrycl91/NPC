4b647d0025fe21415e9eed5fb7888e727516b834
Tsa5522 <82578663+Tsa5522@users.noreply.github.com>
2022-10-02 16:49:50 -0400

    Create sensor_cubev1.cs
    sensory system draft.

---
+++ sensor_cubev1.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sensor_cubev1 : MonoBehaviour
+{
+    public uint distance = random.Next(200, 500); //random the sight of the player
+    public float angle = 70; //human sight angle.
+    public float height = 1.5f; //sight height.
+
+    Mesh mesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
